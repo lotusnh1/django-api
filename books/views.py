619a4462953c47a0ddc795b7e02e8cebe13e96a7
niloofar <your_email@example.com>
2025-09-18 09:08:57 +0330

    test deploy

--- books/views.py
+++ books/views.py
@@ -5,3 +5,4 @@ from .serializers import BookSerializer
 class BookListCreateView(generics.ListCreateAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+##this is test
